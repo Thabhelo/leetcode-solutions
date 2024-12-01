7eb1b6d07f754d6e18e31560f605b8a6f48beb94
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2024-12-01 06:34:03 -0600

    Time: 0 ms (100.00%) | Memory: 17.2 MB (5.28%) - LeetSync

---
+++ 692-top-k-frequent-words/top-k-frequent-words.py
@@ -0,0 +1,13 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # Count word frequencies
+        count = Counter(words)
+
+        # Sort by frequency (descending) and lexicographical order (ascending)
+        sorted_words = sorted(count.keys(), key=lambda x: (-count[x], x))
+
+        # Return the top k frequent words
+        return sorted_words[:k]
+
+
+        
