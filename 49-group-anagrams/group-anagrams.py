ca28ea02eb2e7baef9866a8e6dd069de37fe994f
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2025-04-18 17:52:08 -0500

    Time: 8 ms (93.00%) | Memory: 20.6 MB (76.81%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -3,9 +3,8 @@ class Solution:
         output = {}
         for word in strs:
             sorted_word = "".join(sorted(word))
-            if sorted_word in output:
-                output[sorted_word].append(word)
-            else:
-                output[sorted_word] = [word]
+            if sorted_word not in output:
+                output[sorted_word] = []
+            output[sorted_word].append(word)
         return list(output.values())
 
