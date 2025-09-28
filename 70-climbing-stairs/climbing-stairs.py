7205200e8f64b985861ed3e4109891d7ed154e80
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2025-09-28 02:11:51 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (30.34%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -1,3 +1,13 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        return sum(math.comb(n-k, k) for k in range(n))
+        if n <= 3:
+            return n
+        prev1 = 3
+        prev2 = 2
+        cur = 0
+
+        for _ in range(3, n):
+            cur = prev1 + prev2
+            prev2 = prev1
+            prev1 = cur
+        return cur
