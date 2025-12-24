81070793e6c94d1100f46946e43cb092efcc31b3
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2025-12-24 06:33:43 -0600

    Time: 170 ms (21.94%) | Memory: 77.9 MB (88.51%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,34 +1,56 @@
-class LRUCache:
+class Node:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
+        self.prev = None
+        self.next = None
 
+class LRUCache:
     def __init__(self, capacity: int):
         self.capacity = capacity
-        self.cache = {}  # Stores {key: value}
-        self.keys = []   # Stores [key1, key2...] to track order
+        self.cache = {}  # Maps key -> Node
+
+        # Initialize dummy nodes
+        self.left = Node(0, 0)  # LRU (Least Recently Used)
+        self.right = Node(0, 0) # MRU (Most Recently Used)
+
+        # Connect dummy nodes: [Left] <-> [Right]
+        self.left.next = self.right
+        self.right.prev = self.left
+
+    # Helper: Remove node from linked list
+    def remove(self, node):
+        prev, nxt = node.prev, node.next
+        prev.next = nxt
+        nxt.prev = prev
+
+    # Helper: Insert node at right (Most Recent)
+    def insert(self, node):
+        prev, nxt = self.right.prev, self.right
+        prev.next = node
+        nxt.prev = node
+        node.prev = prev
+        node.next = nxt
 
     def get(self, key: int) -> int:
-        if key not in self.cache:
-            return -1
-        
-        # We used it, so move it to the back (Most Recent)
-        # This is the "expensive" O(N) unoptimized part
-        self.keys.remove(key) 
-        self.keys.append(key)
-        
-        return self.cache[key]
+        if key in self.cache:
+            # We accessed it, so make it Most Recently Used
+            self.remove(self.cache[key])
+            self.insert(self.cache[key])
+            return self.cache[key].value
+        return -1
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            # Update value and move to back
-            self.keys.remove(key)
-            self.keys.append(key)
-            self.cache[key] = value
-        else:
-            # Add new key
-            self.keys.append(key)
-            self.cache[key] = value
-            
-            # Check if we are full
-            if len(self.keys) > self.capacity:
-                # The "Least Recently Used" is at the front (index 0)
-                lru_key = self.keys.pop(0) 
-                del self.cache[lru_key]
+            # If exists, remove old version so we can re-insert as new
+            self.remove(self.cache[key])
+        
+        # Create a new node and insert it at the MRU position
+        self.cache[key] = Node(key, value)
+        self.insert(self.cache[key])
+
+        # If over capacity, remove the LRU node (the one after Left dummy)
+        if len(self.cache) > self.capacity:
+            lru = self.left.next
+            self.remove(lru)
+            del self.cache[lru.key]
