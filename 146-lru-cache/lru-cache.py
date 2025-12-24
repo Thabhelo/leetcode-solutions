d9febaf72afeb83be1d96a810378c4bb1281bb6c
Thabhelo <50872400+Thabhelo@users.noreply.github.com>
2025-12-24 05:09:11 -0600

    Time: 1428 ms (5.00%) | Memory: 77.5 MB (98.55%) - LeetSync

--- 146-lru-cache/lru-cache.py
+++ 146-lru-cache/lru-cache.py
@@ -1,48 +1,34 @@
-class Node:
-    def __init__(self, key, val):
-        self.key, self.val = key, val
-        self.prev = self.next = None
-
-
 class LRUCache:
+
     def __init__(self, capacity: int):
-        self.cap = capacity
-        self.cache = {}  # map key to node
-
-        self.left, self.right = Node(0, 0), Node(0, 0)
-        self.left.next, self.right.prev = self.right, self.left
-
-    # remove node from list
-    def remove(self, node):
-        prev, nxt = node.prev, node.next
-        prev.next, nxt.prev = nxt, prev
-
-    # insert node at right
-    def insert(self, node):
-        prev, nxt = self.right.prev, self.right
-        prev.next = nxt.prev = node
-        node.next, node.prev = nxt, prev
+        self.capacity = capacity
+        self.cache = {}  # Stores {key: value}
+        self.keys = []   # Stores [key1, key2...] to track order
 
     def get(self, key: int) -> int:
-        if key in self.cache:
-            self.remove(self.cache[key])
-            self.insert(self.cache[key])
-            return self.cache[key].val
-        return -1
+        if key not in self.cache:
+            return -1
+        
+        # We used it, so move it to the back (Most Recent)
+        # This is the "expensive" O(N) unoptimized part
+        self.keys.remove(key) 
+        self.keys.append(key)
+        
+        return self.cache[key]
 
     def put(self, key: int, value: int) -> None:
         if key in self.cache:
-            self.remove(self.cache[key])
-        self.cache[key] = Node(key, value)
-        self.insert(self.cache[key])
-
-        if len(self.cache) > self.cap:
-            # remove from the list and delete the LRU from hashmap
-            lru = self.left.next
-            self.remove(lru)
-            del self.cache[lru.key]
-
-# Your LRUCache object will be instantiated and called as such:
-# obj = LRUCache(capacity)
-# param_1 = obj.get(key)
-# obj.put(key,value)
+            # Update value and move to back
+            self.keys.remove(key)
+            self.keys.append(key)
+            self.cache[key] = value
+        else:
+            # Add new key
+            self.keys.append(key)
+            self.cache[key] = value
+            
+            # Check if we are full
+            if len(self.keys) > self.capacity:
+                # The "Least Recently Used" is at the front (index 0)
+                lru_key = self.keys.pop(0) 
+                del self.cache[lru_key]
